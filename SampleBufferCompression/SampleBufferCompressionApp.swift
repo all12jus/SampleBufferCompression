5e32273ffe541579a953f45536c328b2b33901eb
Justin Allen <all12jus@gmail.com>
2024-03-28 09:00:41 -0400

    Initial Commit

---
+++ SampleBufferCompression/SampleBufferCompressionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleBufferCompressionApp.swift
+//  SampleBufferCompression
+//
+//  Created by Justin Allen on 3/28/24.
+//
+
+import SwiftUI
+
+@main
+struct SampleBufferCompressionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
